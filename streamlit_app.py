9b60a7bf85577aedaa042a8ad37c031bc23f1bc4
Arivindraj <35523309+Arivindraj@users.noreply.github.com>
2022-06-25 21:14:57 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ streamlit.dataframe(fruityvice_normalized)
 # test to query our metadata
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * FROM fruit_load_list)")
+my_cur.execute("select * from fruit_load_list")
 my_data_row = my_cur.fetchone()
 streamlit.text("This Fruit load list contains:")
 streamlit.text(my_data_row)
